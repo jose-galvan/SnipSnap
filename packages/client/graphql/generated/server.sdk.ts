35634c070caab917bfd5f3a1921bc1a113420e23
José Guadalupe Galván Aguilar <guadalupe.galvan1002@gmail.com>
2025-08-22 18:32:14 -0600

    feat(client): graphql setup

---
+++ packages/client/graphql/generated/server.sdk.ts
@@ -0,0 +1,142 @@
+import { gql } from '@apollo/client';
+import * as ApolloReactCommon from '@apollo/client/react';
+import * as ApolloReactHooks from '@apollo/client/react';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+const defaultOptions = {} as const;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  DateTime: { input: any; output: any; }
+};
+
+export type CreateSlugInput = {
+  url: Scalars['String']['input'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createUrl: UrlType;
+};
+
+
+export type MutationCreateUrlArgs = {
+  input: CreateSlugInput;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  mostRecent: Array<UrlType>;
+};
+
+export type UrlType = {
+  __typename?: 'UrlType';
+  clickCount: Scalars['Int']['output'];
+  createdAt: Scalars['DateTime']['output'];
+  id: Scalars['String']['output'];
+  originalUrl: Scalars['String']['output'];
+  slug?: Maybe<Scalars['String']['output']>;
+  updatedAt: Scalars['DateTime']['output'];
+};
+
+export type UrlFieldsFragment = { __typename?: 'UrlType', id: string, slug?: string | null, clickCount: number, originalUrl: string };
+
+export type CreateSlugMutationVariables = Exact<{
+  input: CreateSlugInput;
+}>;
+
+
+export type CreateSlugMutation = { __typename?: 'Mutation', createUrl: { __typename?: 'UrlType', id: string, slug?: string | null, clickCount: number, originalUrl: string } };
+
+export type MostRecentQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type MostRecentQuery = { __typename?: 'Query', mostRecent: Array<{ __typename?: 'UrlType', id: string, slug?: string | null, clickCount: number, originalUrl: string }> };
+
+export const UrlFieldsFragmentDoc = gql`
+    fragment UrlFields on UrlType {
+  id
+  slug
+  clickCount
+  originalUrl
+}
+    `;
+export const CreateSlugDocument = gql`
+    mutation createSlug($input: CreateSlugInput!) {
+  createUrl(input: $input) {
+    ...UrlFields
+  }
+}
+    ${UrlFieldsFragmentDoc}`;
+
+/**
+ * __useCreateSlugMutation__
+ *
+ * To run a mutation, you first call `useCreateSlugMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateSlugMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createSlugMutation, { data, loading, error }] = useCreateSlugMutation({
+ *   variables: {
+ *      input: // value for 'input'
+ *   },
+ * });
+ */
+export function useCreateSlugMutation(baseOptions?: ApolloReactHooks.MutationHookOptions<CreateSlugMutation, CreateSlugMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return ApolloReactHooks.useMutation<CreateSlugMutation, CreateSlugMutationVariables>(CreateSlugDocument, options);
+      }
+export type CreateSlugMutationHookResult = ReturnType<typeof useCreateSlugMutation>;
+export type CreateSlugMutationResult = ApolloReactCommon.MutationResult<CreateSlugMutation>;
+export const MostRecentDocument = gql`
+    query mostRecent {
+  mostRecent {
+    ...UrlFields
+  }
+}
+    ${UrlFieldsFragmentDoc}`;
+
+/**
+ * __useMostRecentQuery__
+ *
+ * To run a query within a React component, call `useMostRecentQuery` and pass it any options that fit your needs.
+ * When your component renders, `useMostRecentQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useMostRecentQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useMostRecentQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<MostRecentQuery, MostRecentQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return ApolloReactHooks.useQuery<MostRecentQuery, MostRecentQueryVariables>(MostRecentDocument, options);
+      }
+export function useMostRecentLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<MostRecentQuery, MostRecentQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return ApolloReactHooks.useLazyQuery<MostRecentQuery, MostRecentQueryVariables>(MostRecentDocument, options);
+        }
+export function useMostRecentSuspenseQuery(baseOptions?: ApolloReactHooks.SkipToken | ApolloReactHooks.SuspenseQueryHookOptions<MostRecentQuery, MostRecentQueryVariables>) {
+          const options = baseOptions === ApolloReactHooks.skipToken ? baseOptions : {...defaultOptions, ...baseOptions}
+          return ApolloReactHooks.useSuspenseQuery<MostRecentQuery, MostRecentQueryVariables>(MostRecentDocument, options);
+        }
+export type MostRecentQueryHookResult = ReturnType<typeof useMostRecentQuery>;
+export type MostRecentLazyQueryHookResult = ReturnType<typeof useMostRecentLazyQuery>;
+export type MostRecentSuspenseQueryHookResult = ReturnType<typeof useMostRecentSuspenseQuery>;
+export type MostRecentQueryResult = ApolloReactCommon.QueryResult<MostRecentQuery, MostRecentQueryVariables>;
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client/react'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Home from './pages/Home'
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: import.meta.env.VITE_SERVER_URL }),
+  cache: new InMemoryCache(),
+})
+
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
